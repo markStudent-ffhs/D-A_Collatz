6331dc9a8fec8b62b8d5a47e7c09285cff9b83df
Mark Achermann <achermann@atfront.ch>
2017-09-12 16:26:13 +0200

    second commit

--- src/ch/ffhs/dua/collatz/Collatz.java
+++ src/ch/ffhs/dua/collatz/Collatz.java
@@ -2,11 +2,12 @@ package ch.ffhs.dua.collatz;
 
 public class Collatz {
 
-	public static int calculateCollatze(int number)
+	public static int calculateCollatze(int number, int counter)
 	{
 		System.out.println(number);
+		
 		if(number == 1) {
-			return number;
+			return counter+1;
 		}
 		
 		if(number % 2 > 0) {
@@ -14,14 +15,16 @@ public class Collatz {
 		} else {
 			number = number / 2;
 		}
-		number = calculateCollatze(number);
+		counter = calculateCollatze(number, counter+1);
 		
-		return number;
+		return counter;
 	}
 	
 	public static void main(String args[])
 	{
-		calculateCollatze(19);
+		int count = calculateCollatze(7, 0);
+		System.out.println("# Anzahl Folgen bis das Ergebnis 1 ist:");
+		System.out.println(count);
 	}
 	
 }
